e90c60fe59ca37e544f969e497e01b66ebd94fcb
Bogdan Krasilnikov <bogdan.krasilnikov@ispring.com>
2025-10-04 16:36:48 +0300

    Добавил GracefullShutdown

--- src/orderservice/main.go
+++ src/orderservice/main.go
@@ -1,14 +1,64 @@
 package main
 
 import (
+	"context"
 	"net/http"
 	"orderservice/transport"
+	"os"
+	"os/signal"
+	"syscall"
+
+	log "github.com/sirupsen/logrus"
 )
 
 func main() {
-	router := transport.Router()
-	err := http.ListenAndServe(":8000", router)
+	log.SetFormatter(&log.JSONFormatter{})
+	file, err := os.OpenFile("orderservice.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err == nil {
+		log.SetOutput(file)
+		defer func(file *os.File) {
+			err := file.Close()
+			if err != nil {
+				return
+			}
+		}(file)
+	}
+
+	serverUrl := ":8000"
+	log.WithFields(log.Fields{"url": serverUrl}).Info("Starting server")
+
+	killSignalChan := getKillSignalChan()
+	srv := startServer(serverUrl)
+
+	waitForKillSignal(killSignalChan)
+	err = srv.Shutdown(context.Background())
 	if err != nil {
 		return
+	}
+}
+
+func startServer(serverUrl string) *http.Server {
+	router := transport.Router()
+	srv := &http.Server{Addr: serverUrl, Handler: router}
+	go func() {
+		log.Fatal(http.ListenAndServe(serverUrl, router))
+	}()
+
+	return srv
+}
+
+func getKillSignalChan() chan os.Signal {
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt, syscall.SIGTERM)
+	return signalChan
+}
+
+func waitForKillSignal(signalChan chan os.Signal) {
+	killSignalChan := <-signalChan
+	switch killSignalChan {
+	case os.Interrupt:
+		log.Info("Got SIGINT...")
+	case syscall.SIGTERM:
+		log.Info("Got SIGTERM...")
 	}
 }
--- src/orderservice/transport/handlers.go
+++ src/orderservice/transport/handlers.go
@@ -5,17 +5,32 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	log "github.com/sirupsen/logrus"
 )
 
-func Router() *mux.Router {
+func Router() http.Handler {
 	r := mux.NewRouter()
 	s := r.PathPrefix("/api/v1").Subrouter()
 
 	s.HandleFunc("/hello-world", helloWorld).Methods(http.MethodGet)
-
-	return r
+	return logMiddleware(r)
 }
 
 func helloWorld(w http.ResponseWriter, _ *http.Request) {
-	fmt.Fprintln(w, "Hello World")
+	_, err := fmt.Fprintf(w, "Hello World!")
+	if err != nil {
+		return
+	}
+}
+
+func logMiddleware(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.WithFields(log.Fields{
+			"method":     r.Method,
+			"url":        r.URL,
+			"remoteAddr": r.RemoteAddr,
+			"userAgent":  r.UserAgent(),
+		}).Info("got a new request")
+		h.ServeHTTP(w, r)
+	})
 }
